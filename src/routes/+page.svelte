18ad64e3fa096f16aed368849d6aeb6ec5347d9d
Hans Melgers <hans.melgers@gmail.com>
2023-09-03 12:28:17 +0200

    First component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
+<script>
+    import Hello from "$lib/demo/Hello.svelte";
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<Hello/>
+<Hello name="Hans"/>
